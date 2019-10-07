bef406222551326165f4d159ee65496e8b7f857a
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-10-07 23:36:31 +0200

    Retrieve data from response
    The response is in JSON format.
    
    HAL sends JSON text. The webapp must automatically parse it into HalResponse.
    That is why I use the JSonFeature with the Jackson Serializer.

--- src/io/gakusci/gumichan01/ktorpoc/restapi/HalClient.kt
+++ src/io/gakusci/gumichan01/ktorpoc/restapi/HalClient.kt
@@ -1,19 +1,22 @@
 package io.gakusci.gumichan01.ktorpoc.restapi
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.JacksonSerializer
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.get
 
 class HalClient {
 
     private val halUrl = "https://api.archives-ouvertes.fr/search/?q=%s&wt=json"
-    private val client: HttpClient = HttpClient()
+    private val client: HttpClient = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = JacksonSerializer()
+        }
+    }
 
     suspend fun search(query: String): List<HalResultEntry> {
         val url = halUrl.format(query)
-        val response: String = client.get(url)
-        println("=====")
-        println(response)
-        println("===== END")
-        return listOf()
+        return client.get<HalResponse>(url).response.docs ?: listOf()
     }
 }
