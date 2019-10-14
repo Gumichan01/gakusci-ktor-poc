589934eca7e406c8745603f03a75aa542be137ea
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-10-14 23:00:56 +0200

    Fix blocking method call
    The constructor of XMLReader is a blocking method.
    In order avoid blocking the call (that is in a coroutine),
    this commit laucnh if in antother coroutine so the current method is suspended.

--- src/io/gakusci/gumichan01/ktorpoc/domain/service/ArxivClient.kt
+++ src/io/gakusci/gumichan01/ktorpoc/domain/service/ArxivClient.kt
@@ -4,15 +4,20 @@ import com.rometools.rome.io.SyndFeedInput
 import com.rometools.rome.io.XmlReader
 import io.gakusci.gumichan01.ktorpoc.restapi.ArxivResultEntry
 import io.gakusci.gumichan01.ktorpoc.restapi.Author
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.net.URL
 
 class ArxivClient {
 
     private val arxivUrl = "https://export.arxiv.org/api/query?search_query=%s"
 
-    fun search(query: String): List<ArxivResultEntry> {
+    suspend fun search(query: String): List<ArxivResultEntry> {
         val url = arxivUrl.format(query)
-        return SyndFeedInput().build(XmlReader(URL(url))).entries.map { e ->
+        val xmlReader: XmlReader = withContext(Dispatchers.IO) {
+            XmlReader(URL(url))
+        }
+        return SyndFeedInput().build(xmlReader).entries.map { e ->
             ArxivResultEntry(e.authors.map { a -> Author(a.name) }, e.title, e.publishedDate, e.link)
         }
     }
